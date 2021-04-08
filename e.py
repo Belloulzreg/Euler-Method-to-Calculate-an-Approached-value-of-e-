802b2ee547f0f6f1ab742652b383104979a5f39e
REDJIL Abou Bakr Essadiq <52807987+Belloulzreg@users.noreply.github.com>
2021-04-08 18:51:42 +0100

    moved the loop inside a function

--- e.py
+++ e.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 #an algorith to calculate an approximate value of "e" using the Euler methode 
 #to solve ODE (y'-y=0 for y(0)=1 
-y_0 = 1;
-n = 10000000; 
-h = 1 / n; 
-y = [1];
-y.append(1);
-for i in range(0, n+1, 1):
+def Euler(n=10000000): 
+	h = 1 / n; 
+	y = [1];
+	y.append(1);
+	for i in range(0, n+1, 1):
 	
-	y.insert(i+1, y[i]*(1+h));
-print("e =", y[n]);
+		y.insert(i+1, y[i]*(1+h));
+	return y[n];
+print(Euler());
